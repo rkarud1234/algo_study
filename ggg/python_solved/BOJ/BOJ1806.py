7348f62dcb3fae91b9932b394896e3b614a290e5
rkarud1234 <rkarud1324@naver.com>
2026-02-20 09:10:16 +0900

    [Python] BOJ1806 부분합 풀이

--- ggg/python_solved/BOJ/BOJ1806.py
+++ ggg/python_solved/BOJ/BOJ1806.py
@@ -3,19 +3,16 @@ arr = list(map(int, input().split()))
 l = 0
 r = 1
 sum = arr[0]
-cnt = 1
 ans = 1000000001
 
 while l < N and r <= N:
     if sum >= S:
-        ans = min(ans, cnt)
+        ans = min(ans, r-l)
         sum -= arr[l]
         l += 1
-        cnt -= 1
     elif r < N: # 마지막 인덱스를 검사하기 위한 조건
         sum += arr[r]
         r += 1
-        cnt += 1
     else:
         l += 1
 
