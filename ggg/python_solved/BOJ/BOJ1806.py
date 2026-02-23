6a961c2535a45ece51c692d630f330276ffd07d7
ggg <84266499+rkarud1234@users.noreply.github.com>
2026-02-24 08:52:37 +0900

    [백준] 1806. 부분합 문제풀이 (#31)
    * [Python] BOJ1806 부분합 풀이
    
    * [Python] BOJ1806 부분합 풀이
    
    * [Python] BOJ1806 부분합 풀이

---
+++ ggg/python_solved/BOJ/BOJ1806.py
@@ -0,0 +1,19 @@
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+l = 0
+r = 1
+sum = arr[0]
+ans = 1000000001
+
+while l < N and r <= N:
+    if sum >= S:
+        ans = min(ans, r-l)
+        sum -= arr[l]
+        l += 1
+    elif r < N: # 마지막 인덱스를 검사하기 위한 조건
+        sum += arr[r]
+        r += 1
+    else:
+        l += 1
+
+print(ans if ans != 1000000001 else 0)
