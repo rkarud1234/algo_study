10734a45128ae7832e58a0cac5cec6f1a69c7c23
10153192 <dongjun.min@kt.com>
2026-03-12 10:49:18 +0900

    [boj]1202. 보석 도둑  문제풀이

--- mdj/boj/1202.py
+++ mdj/boj/1202.py
@@ -1,37 +1,34 @@
-import bisect
+import heapq
 import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
-switch = lambda x : [-1 * int(x[1]), int(x[0])]
-jewelrys = [switch(input().split()) for _ in range(N)]
+# switch = lambda x : [-1 * int(x[1]), int(x[0])]
+jewelrys = [list(map(int, input().split())) for _ in range(N)]
 jewelrys.sort()
 
 bags = [int(input()) for _ in range(K)]
-bag_dict = {i:1 for i in range(K)}
+bag_dict = {i:[] for i in range(K)}
 bags.sort()
 
+jewelrys_candidate = []
+jc_idx = 0
+
 answer = 0
-for jv, jm in jewelrys:
-    if bags==[]:
-        break
-
-    idx = bisect.bisect_left(bags, jm)
-    
-    if idx == K:
-        continue
-    
-    while bag_dict[idx]==0:
-        idx += 1
-        if idx >= K:
-            idx -= 1
+for bag in bags:
+    while True:
+        if jc_idx >= N:
             break
-
-    if bag_dict[idx]:
-        bag_dict[idx] = 0
-        answer += jv * -1
+        mi, vi = jewelrys[jc_idx]
+        if mi <= bag:
+            heapq.heappush(jewelrys_candidate, -1 * vi)
+            jc_idx += 1
+        else:
+            break
+    
+    if jewelrys_candidate:
+        answer += heapq.heappop(jewelrys_candidate) * -1
+    
 
 print(answer)
-
-
