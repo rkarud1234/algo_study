8e918c3b0e165e2c60be207e586c64ad75d4ab32
rkarud1234 <rkarud1324@naver.com>
2026-03-26 16:07:49 +0900

    [Python] BOJ1509 팰린드롬 분할 풀이

---
+++ ggg/python_solved/BOJ/BOJ1509.py
@@ -0,0 +1,21 @@
+s = input()
+LEN = len(s)
+arr = [LEN] * (LEN+1)
+arr[LEN] = 0
+
+def palindrome(l, r):
+    while l >= 0 and r < LEN:
+        if s[l] != s[r]:
+            return(l+1, r-1)
+        arr[r] = min(arr[l-1] + 1, arr[r])
+        l = l-1
+        r = r+1
+    return (l+1, r-1)
+
+for i in range(LEN):
+    palindrome(i, i) # 홀수인 경우
+    palindrome(i-1, i) # 짝수인 경우
+
+    arr[i] = min(arr[i-1] + 1, arr[i])
+
+print(arr[LEN-1])
